f8b75dcac58541a7d21bb677ebf879cbcdc8ebc6
FirstHassan24 <akilou.issifou23@gmail.com>
2026-01-30 11:06:22 -0500

    end:me and mentore made set upn the foundations for out project

---
+++ board/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+#trello list such as todo or in progress
+class List(models.Model):
+    name = models.TextField(max_length=30)
+
+#cards in a list, such as "make migrations" in the "TO DO" List
+class Item(models.Model):
+    list= models.ForeignKey(List, on_delete=models.CASCADE)
+    title = models.TextField(max_length=200,)
+    description = models.TextField(null=True)
+    pass
