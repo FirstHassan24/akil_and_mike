4fd9504d8084802bdd11022b89f999d9c621f975
FirstHassan24 <akilou.issifou23@gmail.com>
2026-02-03 10:09:17 -0500

    Add TODOs for configuring new Django app and modeling organizations and employees

--- board/models.py
+++ board/models.py
@@ -13,3 +13,10 @@ class Item(models.Model):
     title = models.TextField(max_length=200,)
     description = models.TextField(null=True)
     pass
+
+#TODO:Configure a new django app, set up the models, migrate the database, and test your models in the shell for the following app:Model organizations and their employees. An employee can only work for one organization at a time. This modeling should be basically identical to what we did with Trello!
+#1.DRAW an erd of the employee and the organization
+#2.set up the models(link the foreign key:)
+# 3.migrate the models
+# 4.test the model in the shell 
+#  5. YOUR DONE
