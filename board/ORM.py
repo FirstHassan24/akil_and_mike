0c30a9dc7c9e9269eea28cc1e0b183934ac29063
FirstHassan24 <akilou.issifou23@gmail.com>
2026-03-06 16:14:00 -0500

    Refactor TODO comments in ORM.py for improved clarity and guidance on item deletion process

--- board/ORM.py
+++ board/ORM.py
@@ -1,8 +1,8 @@
 #TODO:1. For convenience, start by writing code that loops over all of your items and deletes them, then loops over all of your lists and deletes them. You can then run this code in order from the start and get the same result each time:
-#1. in the shell import the models,
-#2. loop over the first model
-from .models import Item,List
-
+#1. catches all the rows in each model
+#2. evaluate each row one by one
+#3 delete each value as you loop through them:
+#for some reason 
 items = Item.objects.all()
 lists = List.objects.all()
 for item in items:
