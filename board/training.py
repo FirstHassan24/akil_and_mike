2e687b90181d35d7cfaad25f1d769cb97f536979
FirstHassan24 <akilou.issifou23@gmail.com>
2026-03-23 15:32:10 -0400

    Fix filter and get methods in training.py for item and list queries

--- board/training.py
+++ board/training.py
@@ -1,8 +1,8 @@
 # 1. Get all items that belong to Groceries
-Items.objects.fillter(Groceries)
+Item.objects.filter(list= Groceries)
 # 2. Get the one list named Groceries
-List.objects.get(Groceries)
+List.objects.get(name="Groceries")
 # 3. Check whether any item named eggs exists
-Item.objects.exists(eggs)
+Item.objects.filter(name="eggs").exists()
 # 4. Get all items named eggs
-Item.objects.filter(eggs)
+Item.objects.filter(name="eggs")
