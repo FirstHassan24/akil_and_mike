956816b2a69712431cea325d176d9379dbcb0d42
FirstHassan24 <akilou.issifou23@gmail.com>
2026-04-21 16:03:10 -0400

    Add items to the "Done" list in homework.py

--- board/homework.py
+++ board/homework.py
@@ -25,6 +25,10 @@ def run():
     #TODO:3.in "Done", create three items: "Eat breakfast", "Eat lunch", and "Shower". Do this by finding the List by filtering on its id (consider using .get) and storing it in a variable, then using the list's `item_set` relation. Remember: if you use the item_set relation, you don't have to pass a list argument when creating or filtering on an item, as they are already related to a specific list.
     #create the breakfast variable in items:
     breakfast=Item(name="Eat Breakfast")
+    #create the lunch variable in items:
+    lunch=Item(name="Eat lunch")
+    #create the shower variable in  item:
+    
     #use the list variable from the entry model to link breakfast with done:
     breakfast.list=done
     #save it:
